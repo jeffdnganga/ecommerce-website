0c3c3b983057d30c816fd97daba7dc23db672f3e
jeffdnganga <jeffdaniels950@gmail.com>
2021-10-07 15:41:42 +0300

    Edits in footer done

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="panel panel-default bg-light">
     <div class="panel-body">
-        Panel Content
+        Footer Content
     </div>
     <div class="panel-footer bg-secondary">Panel footer</div>
 </div>
